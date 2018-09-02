dbe4960f8f6477d16a94bc3370f69bca6040ba0c
SrividyaKK <SrividyaKK@users.noreply.github.com>
2018-09-02 20:41:00 +0530

    Made call and delete visible

--- app/src/main/java/universe/sk/syndriveapp/ContactsActivity.java
+++ app/src/main/java/universe/sk/syndriveapp/ContactsActivity.java
@@ -14,7 +14,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
