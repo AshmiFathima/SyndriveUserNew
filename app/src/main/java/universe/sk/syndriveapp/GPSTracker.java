2c55532b8fa24e20e52b5e616fa31eac11a62b0d
Megha K.C <meghakc10@gmail.com>
2018-10-21 22:29:59 +0530

    Resolving bugs in sending current location via SMS

--- app/src/main/java/universe/sk/syndriveapp/GPSTracker.java
+++ app/src/main/java/universe/sk/syndriveapp/GPSTracker.java
@@ -1,12 +1,14 @@
 package universe.sk.syndriveapp;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
+import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
@@ -33,15 +35,16 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GPSTracker extends Service implements LocationListener{
-
+public class GPSTracker extends Service implements LocationListener {
 
 
     private Context mContext;
 
     private List<String> hospitalAddresses;
 
-    public List<String> getHospitalAddress() { return hospitalAddresses; }
+    public List<String> getHospitalAddress() {
+        return hospitalAddresses;
+    }
 
     // flag for GPS status
     boolean isGPSEnabled = false;
@@ -70,11 +73,28 @@ public class GPSTracker extends Service implements LocationListener{
         location = getLocation();
     }
 
+    @SuppressLint("MissingPermission")
     public Location getLocation() {
 
         try {
 
             locationManager = (LocationManager) mContext.getSystemService(LOCATION_SERVICE);
+           // locationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
+            // Define a listener that responds to location updates
+            LocationListener locationListener = new LocationListener() {
+                public void onLocationChanged(Location location) {
+                    // Called when a new location is found by the network location provider.
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+                    Log.d("location","latitude:"+latitude+"longitude:"+longitude);
+                }
+
+                public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+                public void onProviderEnabled(String provider) {}
+
+                public void onProviderDisabled(String provider) {}
+            };
 
             // getting GPS status
             isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
@@ -83,26 +103,48 @@ public class GPSTracker extends Service implements LocationListener{
             isNetworkEnabled = locationManager
                     .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
-            if (!isGPSEnabled && !isNetworkEnabled) {
+            if (!isGPSEnabled && !isNetworkEnabled)
+            {
                 // no network provider is enabled
+                Log.d("Check:","Show settings alert!");
                 showSettingsAlert();
-            } else {
+            }
+            else
+            {
                 this.canGetLocation = true;
+                Log.d("Location","Network or GPS on!!"+"GPS:"+isGPSEnabled+" Network:"+isNetworkEnabled);
                 // First get location from Network Provider
-                if (isNetworkEnabled ) {
+                if (isNetworkEnabled)
+                {
 
+                    Log.d("if condition:","Entered");
+//                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+//                    {
+//                        // TODO: Consider calling
+//                        //    ActivityCompat#requestPermissions
+//                        // here to request the missing permissions, and then overriding
+//                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//                        //                                          int[] grantResults)
+//                        // to handle the case where the user grants the permission. See the documentation
+//                        // for ActivityCompat#requestPermissions for more details.
+//                        Log.d("NetworkEnabled?","Returning null:(");
+//                        return null;
+//                    }
+                    Log.d("IsNetworkEnabled?","Not Returning null:)");
                     locationManager.requestLocationUpdates(
                             LocationManager.NETWORK_PROVIDER,
                             MIN_TIME_BW_UPDATES,
                             MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
 
                     Log.e("Network", "Network");
-                    if (locationManager != null) {
+                    if (locationManager != null)
+                    {
                         Log.e("Network", "Manager Null");
                         location = locationManager
                                 .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
 
-                        if (location != null) {
+                        if (location != null)
+                        {
                             latitude = location.getLatitude();
                             longitude = location.getLongitude();
                         }
@@ -110,31 +152,46 @@ public class GPSTracker extends Service implements LocationListener{
                 }
 
                 // if GPS Enabled get lat/long using GPS Services
-                if (isGPSEnabled) {
-                    if (location == null ) {
+                if (isGPSEnabled)
+                {
+                    Log.d("location:","entered if block and location = "+location);
+
+                    if (location == null)
+                    {
+//                        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+//                        {
+//                            Log.d("GPSpermission","NO");
+//                            //showSettingsAlert();
+//                            return null;
+//                        }
+                        Log.d("Checking","Reached here!");
                         locationManager.requestLocationUpdates(
                                 LocationManager.GPS_PROVIDER,
                                 MIN_TIME_BW_UPDATES,
                                 MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
 
-                        Log.d("GPS Enabled", "GPS Enabled");
-                        if (locationManager != null) {
-                            location = locationManager
-                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                        Log.d("GPSEnabled", "Location manager = "+locationManager);
 
-                            if (location != null) {
+                        if (locationManager != null)
+                        {
+                            //Criteria criteria = new Criteria();
+                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);//not working
+                            //location = locationManager.getLastKnownLocation(locationManager.getBestProvider(criteria,false));
+                            Log.d("Checkpoint1","location = "+location);
+                            if (location != null)
+                            {
                                 latitude = location.getLatitude();
                                 longitude = location.getLongitude();
+                                Log.d("Checkpoint","latitude = "+latitude+"logitude = "+longitude);
                             }
                         }
                     }
                 }
             }
-
+            locationManager.removeUpdates(locationListener);
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return location;
     }
 
